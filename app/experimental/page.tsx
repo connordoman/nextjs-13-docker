9088d6453c90078f8db4fdf6d3648221d3d37604
connor@connordoman.dev <connor@connordoman.dev>
2023-10-02 19:06:54 -0700

    patternfly installed

---
+++ app/experimental/page.tsx
@@ -0,0 +1,14 @@
+/*
+ * Created on Mon Oct 02 2023
+ * Author: Connor Doman
+ */
+
+import { ActionListSingleGroup } from "../_components/patternfly/ActionList";
+
+import { NextPage } from "next";
+
+const ExperimentalPage: NextPage = () => {
+    return <ActionListSingleGroup />;
+};
+
+export default ExperimentalPage;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+        // Important: return the modified config
+        return config;
+    },
+    experimental: {
+        appDir: true,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
